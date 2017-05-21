815c0f7c9a5b0668e5355482bf988dec2b755701
Alex Miller <apmiller108@yahoo.com>
2017-05-20 20:28:58 -0400

    Fix title expectation to match the expected output

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,14 @@ describe('AppComponent', () => {
   it(`should have as a title`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Welcome to the Trim Angular 2 starter template');
+    expect(app.title).toEqual('NASA Nerd');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to the Trim Angular 2 starter template');
+    expect(compiled.querySelector('h1').textContent)
+      .toContain('NASA Nerd');
   }));
 });
